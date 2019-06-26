caffd589e5aae91eb4b503aefbf2ac876995f966
CYBER SEC <51013762+CYBERSECARYAN@users.noreply.github.com>
2019-06-26 13:34:36 +0530

    Add files via upload

---
+++ C-Linux.sh
@@ -0,0 +1,141 @@
+echo " "
+echo " "
+echo " "
+echo -e "\033[1m \e[31m =========[ All In One Cerberus Linux Package ]========="
+echo " "
+echo " "
+echo " "
+echo -e "\033[1m \e[37m This script is written by [ VENOM-SEC ]====( FROM )====[ CYBER-SEC ] "
+echo " "
+echo " "
+echo -e "\033[1m \e[37m [ Indian CYBER-SEC Team ]=====> ( ANDY-HOOK )---( ABHISHEK RAJ MISHRA )---( TABREZ MALIK )---( VENOM SEC ) "
+echo " "
+echo " "
+echo " "
+echo -e "\033[1m \e[37m NOTICE !!!  ---------->  This script is noly for WSL [ Windows Subsystem Linux ] [ WINDOWS - 10 ] "
+echo " "
+echo " "
+echo " "
+echo " "
+echo -e "\033[1m \e[32m INSTALLATION STARTED [=============>....................................................] "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo -e "\033[1m \e[32m INTERNET DETECTED ! [###########################################........................] "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+apt-get update -y
+echo " "
+apt-get upgrade -y
+echo " "
+apt-get install figlet -y
+echo " "
+apt-get install ruby -y
+echo " "
+gem install lolcat
+echo " "
+apt-get install perl -y
+echo " "
+apt-get install c -y
+echo " "
+apt-get install c++ -y
+echo " "
+apt-get install nano -y
+echo " "
+apt-get install vim -y
+echo " "
+apt-get install tar -y
+echo " "
+apt-get install rar -y
+echo " "
+apt-get install unrar -y
+echo " "
+apt-get install less -y
+echo " "
+apt-get install nmap -y
+echo " "
+apt-get install hydra -y
+echo " "
+apt-get install gnome-terminal -y
+echo " "
+apt-get install openjdk-8-jdk -y
+echo " "
+apt-get install mingw-w64 -y
+echo " "
+apt-get install python2 -y
+echo " "
+apt-get install php -y
+echo " "
+apt-get install python3 -y
+echo " "
+apt-get install git -y
+echo " "
+apt-get install curl -y
+echo " "
+apt-get install wget -y
+echo " "
+apt-get install zipalign -y 
+echo " "
+apt-get install openssl -y
+echo " "
+apt-get install apache2 -y
+echo " "
+apt-get install postgresql -y
+echo " "
+apt-get install unzip -y
+echo " "
+apt-get install zip -y
+echo " "
+apt-get install xterm -y
+echo " "
+apt-get install monodevelop -y 
+echo " "
+apt-get install gcc compiler -y
+echo " "
+apt-get install upx -y
+echo " "
+apt-get install proguard -y
+echo " "
+apt-get install dex2jar -y
+echo " "
+apt-get install backdoor-factory -y
+echo " "
+apt-get install dnsutils -y
+echo " "
+git clone https://github.com/offensive-security/exploit-database.git
+echo " "
+apt-get install exploitdb -y
+echo " "
+searchsploit -u
+echo " "
+echo " "
+echo -e "\033[1m \e[32m NOW METASPLOIT WILL INSTALL IN CERBERUS LINUX " | lolcat -a
+echo " "
+echo " "
+echo " "
+echo " "
+apt-get install metasploit-framework -y
+echo " "
+echo " "
+echo "  ██████╗██╗   ██╗██████╗ ███████╗██████╗     ███████╗███████╗ ██████╗ " | lolcat -a
+echo " ██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗    ██╔════╝██╔════╝██╔════╝ " | lolcat -a
+echo " ██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝    ███████╗█████╗  ██║      " | lolcat -a    
+echo " ██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗    ╚════██║██╔══╝  ██║      " | lolcat -a    
+echo " ╚██████╗   ██║   ██████╔╝███████╗██║  ██║    ███████║███████╗╚██████╗ " | lolcat -a
+echo "  ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝    ╚══════╝╚══════╝ ╚═════╝ " | lolcat -a
+echo "---by [ CYBER-SEC ]---"
+echo "---Creator: [ VENOM-SEC ]"
+echo "---Subscribe [ CYBER SEC ] on youtube for more updates--"
+echo " Channel Link :~   [   https://www.youtube.com/channel/UCrmhzytKiy3ddJCPfpGebqg  ] " | lolcat -a
+echo " "
+echo " "
+echo -e "\033[1m \e[31m ===================================================================="
+echo -e "\033[1m \e[32m ------[ SUCCESSFULLY-INSTALLED-ALL-CERBERUS-LINUX-PACKAGES ]--------" | lolcat -a
+echo -e "\033[1m \e[31m ===================================================================="
---
+++ ReadMe.txt
@@ -0,0 +1,30 @@
+# AIO [ ALL IN ONE PACKAGES INSTALLER WITH [ METASPLOIT ] FOR [ CERBERUS LINUX ] IN ( WINDOWS-10 ) ]
+
+
+
+=========[ WINDOWS SUBSYSTEM LINUX ]===========
+
+-----------------------------------------------
+
+this is aio packages installer , through this you can install almost all packages in cerberus linux with metasploit.
+
+
+----------------------------------------------- 
+
+=====================================================
+
+USAGE :~
+
+1.  apt-get install git
+2. git clone https://github.com/CYBERSECARYAN/C-LINUX
+3. cd C-LINUX
+4. bash C-Linux.sh
+
+======================================================
+
+
+# CYBER-SEC ==>  https://www.youtube.com/channel/UCrmhzytKiy3ddJCPfpGebqg
+
+
+# SUPPORT US
+=============
